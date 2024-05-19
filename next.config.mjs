7666e0b36526a6a4066e273180d825d3f76e0fdd
Raul <rauledmore98@outlook.com>
2024-05-18 21:42:34 -0500

    Cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS' },
+          { key: 'Access-Control-Max-Age', value: '3600' }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
