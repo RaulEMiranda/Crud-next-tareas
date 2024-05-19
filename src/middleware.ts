a13da8c64ba2170eae73039e45163f129052e56c
Raul <rauledmore98@outlook.com>
2024-05-18 20:46:58 -0500

    Cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,33 @@
 import { NextResponse, NextRequest } from "next/server";
 import { jwtVerify, JWTPayload } from "jose";
 import { match } from "assert";
+import NextCors from "nextjs-cors";
 
 export async function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+
+  response.headers.append("Access-Control-Allow-Credentials", "true");
+  response.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  response.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  response.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
   const tokenJWT = req.cookies.get("tokenSession")?.value;
   const path = req.nextUrl.pathname;
 
-  console.log(path, "hola");
-  
-
-  if (!tokenJWT && (path.startsWith("/tasks") || path.startsWith("/dashboard"))) {
+  if (
+    !tokenJWT &&
+    (path.startsWith("/tasks") || path.startsWith("/dashboard"))
+  ) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
-  if (!tokenJWT && (path.startsWith("/inicio") )) {
+  if (!tokenJWT && path.startsWith("/inicio")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
@@ -28,13 +42,13 @@ export async function middleware(req: NextRequest) {
         new TextEncoder().encode(process.env.SECRET)
       );
 
-      return NextResponse.next();
+      return response;
     } catch (error) {
       return NextResponse.redirect(new URL("/signin", req.url));
     }
   }
 
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {
