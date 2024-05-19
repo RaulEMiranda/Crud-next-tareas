eab56fed69a57584cf0893b887ca9de61da5742e
Raul <rauledmore98@outlook.com>
2024-05-18 20:30:28 -0500

    Cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,42 +1,42 @@
-// import { NextResponse, NextRequest } from "next/server";
-// import { jwtVerify, JWTPayload } from "jose";
-// import { match } from "assert";
+import { NextResponse, NextRequest } from "next/server";
+import { jwtVerify, JWTPayload } from "jose";
+import { match } from "assert";
 
-// export async function middleware(req: NextRequest) {
-//   const tokenJWT = req.cookies.get("tokenSession")?.value;
-//   const path = req.nextUrl.pathname;
+export async function middleware(req: NextRequest) {
+  const tokenJWT = req.cookies.get("tokenSession")?.value;
+  const path = req.nextUrl.pathname;
 
-//   console.log(path, "hola");
+  console.log(path, "hola");
   
 
-//   if (!tokenJWT && (path.startsWith("/tasks") || path.startsWith("/dashboard"))) {
-//     return NextResponse.redirect(new URL("/signin", req.url));
-//   }
+  if (!tokenJWT && (path.startsWith("/tasks") || path.startsWith("/dashboard"))) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
 
-//   if (!tokenJWT && (path.startsWith("/inicio") )) {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
+  if (!tokenJWT && (path.startsWith("/inicio") )) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-//   if (tokenJWT && (path === "/signin" || path === "/signup")) {
-//     return NextResponse.redirect(new URL("/inicio", req.url));
-//   }
+  if (tokenJWT && (path === "/signin" || path === "/signup")) {
+    return NextResponse.redirect(new URL("/inicio", req.url));
+  }
 
-//   if (tokenJWT) {
-//     try {
-//       const { payload } = await jwtVerify(
-//         tokenJWT,
-//         new TextEncoder().encode(process.env.SECRET)
-//       );
+  if (tokenJWT) {
+    try {
+      const { payload } = await jwtVerify(
+        tokenJWT,
+        new TextEncoder().encode(process.env.SECRET)
+      );
 
-//       return NextResponse.next();
-//     } catch (error) {
-//       return NextResponse.redirect(new URL("/signin", req.url));
-//     }
-//   }
+      return NextResponse.next();
+    } catch (error) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/tasks/:path*", "/inicio/:path*", "/signin", "/signup"],
-// };
+export const config = {
+  matcher: ["/tasks/:path*", "/inicio/:path*", "/signin", "/signup"],
+};
